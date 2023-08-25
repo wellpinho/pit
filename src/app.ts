317414c7371d0852df841f8b1b7f5b432f42cfda
wellpinho <wellpinho@outlook.com>
2023-08-24 21:12:23 -0300

    server started

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+require("dotenv").config();
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import { AppError } from "./errors/AppError";
+import { routes } from "./routes";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: "Error",
+      message: error.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: "Error",
+    message: "Internal Server Error",
+  });
+});
+
+export { app };
---
+++ src/errors/AppError.ts
@@ -0,0 +1,11 @@
+class AppError {
+  public readonly message: string;
+  public readonly statusCode: number;
+
+  constructor(message: string, statuCode = 400) {
+    this.message = message;
+    this.statusCode = statuCode;
+  }
+}
+
+export { AppError };
